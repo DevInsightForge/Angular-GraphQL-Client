bd6bce78815caa24fae52c566e0cdcb21af61799
Mofajjal Rasul <mofajjalrasul@gmail.com>
2023-04-26 13:30:10 +0600

    login: added login form
    Signed-off-by: Mofajjal Rasul <mofajjalrasul@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
+import { ChatsComponent } from "./components/chats/chats.component";
+import { MessagesComponent } from "./components/messages/messages.component";
 import { ApolloClientModule } from "./core/apollo-client.module";
 import { AppRoutingModule } from "./core/app-routing.module";
 import { HomeComponent } from "./pages/home/home.component";
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { ChatsComponent } from './components/chats/chats.component';
-import { MessagesComponent } from './components/messages/messages.component';
+import { LoginComponent } from "./pages/login/login.component";
+import { RegisterComponent } from "./pages/register/register.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, LoginComponent, RegisterComponent, ChatsComponent, MessagesComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ChatsComponent,
+    MessagesComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     ApolloClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent {
+  constructor(private formBuilder: FormBuilder) {}
 
+  loginForm = this.formBuilder.group({
+    email: "admin@admin.com",
+    password: "Admin@123",
+  });
+
+  login() {
+    console.log(this.loginForm.value);
+  }
 }
