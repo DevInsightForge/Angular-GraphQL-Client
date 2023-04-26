46b50386afc1ad1a9d832d72f329cfc92d2dc076
Mofajjal Rasul <mofajjalrasul@gmail.com>
2023-04-26 12:38:09 +0600

    apollo: created client module for graphql operations
    Signed-off-by: Mofajjal Rasul <mofajjalrasul@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ApolloClientModule } from '../core/apollo-client.module';
 import { AppRoutingModule } from '../core/app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ApolloClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/core/apollo-client.module.ts
@@ -0,0 +1,52 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { InMemoryCache, from, split } from '@apollo/client/core';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { Apollo, ApolloModule } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { createClient } from 'graphql-ws';
+
+const BASE_URL = isDevMode()
+  ? 'http://localhost:4000/graphql'
+  : 'https://imzihad21.is-a.dev/graphql';
+
+@NgModule({
+  exports: [ApolloModule],
+})
+export class ApolloClientModule {
+  constructor(apollo: Apollo, httpLink: HttpLink) {
+    // Create an http link:
+    const http = httpLink?.create({
+      uri: BASE_URL,
+    });
+
+    // Create a WebSocket link:
+    const websocket = new GraphQLWsLink(
+      createClient({
+        url: BASE_URL.replace('http', 'ws'),
+      })
+    );
+
+    const connectionLink = split(
+      // split based on operation type
+      ({ query }) => {
+        const { kind, operation }: any = getMainDefinition(query);
+        return kind === 'OperationDefinition' && operation === 'subscription';
+      },
+      websocket,
+      http
+    );
+
+    apollo.create({
+      link: from([connectionLink]),
+      cache: new InMemoryCache(),
+      credentials: 'include',
+      defaultOptions: {
+        watchQuery: {
+          fetchPolicy: 'network-only',
+          errorPolicy: 'all',
+        },
+      },
+    });
+  }
+}
