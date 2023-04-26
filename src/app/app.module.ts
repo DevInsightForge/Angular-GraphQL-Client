603852647e1729cd6dae69b6660cccdbe24f4c37
Mofajjal Rasul <mofajjalrasul@gmail.com>
2023-04-26 13:00:35 +0600

    template: added page and components templates
    Signed-off-by: Mofajjal Rasul <mofajjalrasul@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,13 @@ import { AppComponent } from "./app.component";
 import { ApolloClientModule } from "./core/apollo-client.module";
 import { AppRoutingModule } from "./core/app-routing.module";
 import { HomeComponent } from "./pages/home/home.component";
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ChatsComponent } from './components/chats/chats.component';
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent, RegisterComponent, ChatsComponent, MessagesComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/core/app-routing.module.ts
+++ src/app/core/app-routing.module.ts
@@ -1,11 +1,31 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomeComponent } from "../pages/home/home.component";
+import { LoginComponent } from "../pages/login/login.component";
+import { RegisterComponent } from "../pages/register/register.component";
 
 const routes: Routes = [
   {
     path: "",
-    component: HomeComponent,
+    children: [
+      {
+        path: "",
+        component: HomeComponent,
+      },
+    ],
+  },
+  {
+    path: "",
+    children: [
+      {
+        path: "login",
+        component: LoginComponent,
+      },
+      {
+        path: "register",
+        component: RegisterComponent,
+      },
+    ],
   },
 ];
 
