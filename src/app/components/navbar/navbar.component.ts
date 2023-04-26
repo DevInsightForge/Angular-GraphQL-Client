3facc1680ddfb5a809a117704df914a93a24af2d
Mofajjal Rasul <mofajjalrasul@gmail.com>
2023-04-26 17:09:47 +0600

    nav: show user email when authenticated
    Signed-off-by: Mofajjal Rasul <mofajjalrasul@gmail.com>

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Select } from "@ngxs/store";
+import { Observable } from "rxjs";
+import { User } from "../../../generated/graphql";
+import { AuthUserState } from "../../store/user/auth-user.state";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"],
 })
 export class NavbarComponent {
-
+  @Select(AuthUserState.user) user$: Observable<User> | undefined;
 }
