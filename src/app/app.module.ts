b7945cb347bd55400d477d745c839185d363b1a1
Mofajjal Rasul <mofajjalrasul@gmail.com>
2023-04-26 12:44:25 +0600

    home: added home page component
    Signed-off-by: Mofajjal Rasul <mofajjalrasul@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ApolloClientModule } from '../core/apollo-client.module';
-import { AppRoutingModule } from '../core/app-routing.module';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { ApolloClientModule } from "./core/apollo-client.module";
+import { AppRoutingModule } from "./core/app-routing.module";
+import { HomeComponent } from "./pages/home/home.component";
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ApolloClientModule],
+  declarations: [AppComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ApolloClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
