0effb2282bdb7a32bc1430f180c8e2d67bbd615d
Mofajjal Rasul <mofajjalrasul@gmail.com>
2023-04-27 09:53:38 +0600

    guards: fixed guard logic for observable value
    Signed-off-by: Mofajjal Rasul <mofajjalrasul@gmail.com>

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,16 +1,21 @@
 import { inject } from "@angular/core";
 import { Router } from "@angular/router";
 
+import { map } from "rxjs";
 import { AuthService } from "./auth.service";
 
 export const authGuard = () => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  if (authService.isAuthenticated) {
-    return true;
-  }
+  return authService?.isAuthenticated?.pipe(
+    map((check) => {
+      if (Boolean(check)) {
+        return true;
+      }
 
-  // Redirect to the login page
-  return router.parseUrl("/login");
+      // Redirect to the login page
+      return router.parseUrl("/login");
+    })
+  );
 };
